e923882e7775d68cf905e90197b4cfa990c2787a
Victor <vidrums86@gmail.com>
2026-03-05 21:53:13 +0000

    fix: changed PORT on server for production

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 import flashCardsRouter from "./routes/flashcards.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
